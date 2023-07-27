6afee12debf4052550d08c695d51800b37438fc7
Zac-Baker91 <zbaker.it@gmail.com>
2023-07-26 20:28:14 -0400

    added player json and restructured player related classes around json's format.

---
+++ src/Abilities.java
@@ -0,0 +1,25 @@
+public class Abilities {
+    private final String name;
+    private final String type;
+    //cost from stat pool of [type]
+    private final int cost;
+
+    public Abilities(String name, String type, int cost) {
+        this.name = name;
+        this.type = type;
+        this.cost = cost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+}
--- src/Player.java
+++ src/Player.java
@@ -1,39 +1,30 @@
-import java.util.ArrayList;
-import java.util.HashMap;
+
 
 public class Player {
-
-    //There are 3 base Skill Might Speed and Intellect.
-    //each has a Pool and Edge. Pool is your capacity and edge is your skill level.
-    private class Skill{
-        private int currentPool;
-        private int edge;
-        private int maxPool;
-        public Skill(){
-            currentPool = -1;
-            maxPool = -1;
-            edge = -1;
-        }
-        public Skill(int p,int e){
-            currentPool = p;
-            maxPool = p;
-            edge = e;
-        }
-
-
-
-    }
-    private Skill might, speed, intellect;
-
-    //Tier is players level and effort is used make action easier
+    private final String name;
+    private  String Description;
+    //There are 3 words that will define a player's character [descriptor, type, focus]
+    private final String descriptor, type, focus;
+    //There are 3 base Stats Might Speed and Intellect.
+    private Stat Might, Speed, Intellect;
+    //Tier is players level and effort is used to modify the challenge level of a die roll.
     private int tier,effort,xp;
-    private Action[] actionArray = new Action[36];
+    // 36 is the number of skill slots provided on the character sheet provide with the rule book
+    private Skill[] statsArray = new Skill[36];
+    //Abilities are special so they are limited to 10
+    private Abilities[] abilitiesArray = new Abilities[10];
+    //This is not a game for loot goblins
+    private Item[] playersBag = new Item[10];
+    //meant for quick use hold to many and thing get weird....
+    private Cypher[] cypherArray = new Cypher[4];
 
-    public Player(String name, String descriptor,String type,String focus){
+
+    public Player(String name, String descriptor, String type, String focus) {
+        this.name = name;
+        this.descriptor = descriptor;
+        this.type = type;
+        this.focus = focus;
+        //todo !!!this is where character generation gets cool!!!
         System.out.println(name + " the " + descriptor +" "+ type+" who " + focus + " has been born!");
-
     }
-
-
-
 }
