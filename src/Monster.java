8b58889d2c5ca38d543a3e1224caa00f142043dc
Zac-Baker91 <135909475+Zac-Baker91@users.noreply.github.com>
2024-08-15 11:03:40 -0400

    Create Monster.java
    started the monster object based on claude monster generation

---
+++ src/Monster.java
@@ -0,0 +1,21 @@
+public class Monster {
+  private final String name;
+  private final String description;
+  private final String motive;
+  private final String environment;
+  private final String movment;
+  private final int level;
+  private int health;
+  private final int damage;
+  private final int armor;
+  private final String[] modifications;
+  private final Object[] combat;
+  private final String interaction;
+  private final String use;
+  private Object[] loot;
+  private final String intrusion;
+  private Object[] special_abilities;
+  
+  
+  }
+
