db908c7518b59d51815e8b775761dfd90d8a4081
Zac-Baker91 <135909475+Zac-Baker91@users.noreply.github.com>
2024-08-15 08:35:57 -0400

    Update Focus.java
    Changed focus to match claude json

--- src/playerMeta/Focus.java
+++ src/playerMeta/Focus.java
@@ -3,8 +3,7 @@ package playerMeta;
  * Record that contains arrays of ability's by tier 1-6.
  * These Foci are highly unique to the game and the players, They should are constructed using user input.
  **/
-public record Focus(String name,Abilities[] tierOne,Abilities[] tierTwo,Abilities[] tierThree,
-                    Abilities[] tierFour,Abilities[] tierFive,Abilities[] tierSix) {
+public record Focus(String name,String description,String[] connectionOptions,String[] additionalEquipment, Abilities[] Abilities_array) {
     @Override
     public String toString(){
         return this.name();
